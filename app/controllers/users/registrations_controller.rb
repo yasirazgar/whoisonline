27c530bd4d99abc566f50711afcd6e37242d9875
Mohammed Yasir Azgar <yasirazgar@gmail.com>
2017-03-19 14:10:12 +0530

    fixing helper path [ci skip]

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  include UserStatusHelper
+  include Users::UserStatusHelper
   after_filter :update_status_online, :only => [:create]
 # before_action :configure_sign_up_params, only: [:create]
 before_action :configure_account_update_params, only: [:update]
